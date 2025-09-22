893ff9d89a9dd41dc2db9b350fdc9d499286407d
Fatemeh Sadat Hoseini <fatemehoseinii007@gmail.com>
2025-09-22 23:43:00 +0330

    updated my files

--- inedx.js
+++ inedx.js
@@ -1,16 +1,37 @@
+function changeTemperature(response){
+let temperature=response.data.temperature.current;
+let temperatureDegree=document.querySelector("#degree");
+temperatureDegree.innerHTML=Math.round(temperature);
+
+let humidity=document.querySelector("#Humidity");
+humidity.innerHTML=response.data.temperature.humidity;
+
+let wind=document.querySelector("#Wind");
+wind.innerHTML=response.data.wind.speed;
+
+let cityName=document.querySelector(".main-left h1");
+cityName.innerHTML=response.data.country;
+
+let weatherDiscription=document.querySelector("#weather-discription");
+weatherDiscription.innerHTML=response.data.condition.description;
+};
+
+
 function changeUrl(city){
 let myApi='c788bbe4fbob842f25a727737f00tc5e';
 let Url=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${myApi}`;
-}
-function changeCity(event){
-event.preventDefault();
-let textInput=document.querySelector("#text-input");
-let cityName=document.querySelector(".main-left h1");
-cityName.innerHTML=textInput.value;
-changeUrl(textInput.value)
+axios.get(Url).then(changeTemperature);
 };
 
-// axios.get(url).then()
+
+function changeCity(event){
+
+event.preventDefault();
+let textInput=document.querySelector("#text-input");
+// let cityName=document.querySelector(".main-left h1");
+changeUrl(textInput.value)    
+};
+
 
 let searchForm=document.querySelector("#search-form");
 searchForm.addEventListener("submit",changeCity);
