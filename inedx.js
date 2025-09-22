499c08acd7c31b7c68fa8e041835be3ec13fbdea
Fatemeh Sadat Hoseini <fatemehoseinii007@gmail.com>
2025-09-22 22:10:10 +0330

    update JS file

--- inedx.js
+++ inedx.js
@@ -1,11 +1,16 @@
+function changeUrl(city){
+let myApi='c788bbe4fbob842f25a727737f00tc5e';
+let Url=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${myApi}`;
+}
 function changeCity(event){
-
 event.preventDefault();
 let textInput=document.querySelector("#text-input");
-let city=textInput.value;
 let cityName=document.querySelector(".main-left h1");
-cityName.innerHTML=city;
+cityName.innerHTML=textInput.value;
+changeUrl(textInput.value)
 };
+
+// axios.get(url).then()
 
 let searchForm=document.querySelector("#search-form");
 searchForm.addEventListener("submit",changeCity);
